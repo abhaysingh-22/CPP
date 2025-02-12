37f226cbdd5c18e44d6f62e8da16c86c0f7f7bcf
myfirstrepo <abhaysingh6575@gmail.com>
2025-02-13 02:44:06 +0530

    do some changes

--- Practice/q5.cpp
+++ Practice/q5.cpp
@@ -1,3 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int main(){
+    int arr[] = {1, 2, 3, 4, 5};
+    int k = 2;
+    int n = 5;
+
+    k = k % n;
+    int ansarr[5];
+    int j = 0;
+    for(int i = n-k; i < n; i++){
+        ansarr[j++] = arr[i];
+    }
+    for(int i = 0; i <= k; i++){
+        ansarr[j++] = arr[i];
+    }
+    for(int i = 0; i < n; i++){
+        cout << ansarr[i] << " ";
+    }
+    return 0;
+}
