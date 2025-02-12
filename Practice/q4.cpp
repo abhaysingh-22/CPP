670658180bbee784d10dbcb9cac110bf663dbc29
myfirstrepo <abhaysingh6575@gmail.com>
2025-02-12 22:27:14 +0530

    commit changes

--- Practice/q4.cpp
+++ Practice/q4.cpp
@@ -1,4 +1,4 @@
-// bidya quesion h 
+// // bidya quesion h 
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -29,9 +29,76 @@ int main(){
 
     int indexlargest = largenum(arr, n);
 
-    arr[indexlargest] = -1;
+    // arr[indexlargest] = -1;
+
+    int largestelement = arr[indexlargest];
+    for(int i = 0; i < n; i++){
+        if(arr[i] == largestelement){
+            arr[i] = -1;
+        }
+    }
     int secondlargest = largenum(arr, n);
 
     cout << arr[secondlargest] << endl;
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*************if every element in array is unique then you can use this***************
+
+
+
+
+
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int largestnum(int arr[], int n){
+
+//     int max = INT_MIN;
+//     int maxindex = -1;
+
+//     for(int i = 0; i < n; i++){
+//         if(arr[i] > max){
+//             max = arr[i];
+//             maxindex  = i;
+//         }
+//     }
+//     return maxindex;
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     int firstlarge = largestnum(arr, n);
+//     arr[firstlarge] = -1;
+
+//     int secondlarge = largestnum(arr, n);
+
+//     cout << arr[secondlarge] << endl;
+//     return 0;
+// }
