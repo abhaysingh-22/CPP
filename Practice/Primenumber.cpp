f7d8649bd67e9b7c27a1c1756c07ff318a5844ac
myfirstrepo <abhaysingh6575@gmail.com>
2025-02-16 21:07:48 +0530

    Prime Number Problem

---
+++ Practice/Primenumber.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int count = 0;
+
+    for(int i = 1; i <= n ;i++){
+        if(n % i == 0){
+            count++;
+        }
+    }
+
+    if(count == 2){
+        cout << "isPrime" << endl;
+    }else{
+        cout << "notPrime" << endl;
+    }
+    return 0;
+}
