690b276fa0001224a2eb5910fc397bb072598286
myfirstrepo <abhaysingh6575@gmail.com>
2025-02-19 22:24:58 +0530

    addition of arr using recursion

---
+++ Practice/addarrelerecursion.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int f(int arr[], int i, int n){
+
+    if(i == n-1){
+        return arr[i];
+    }
+
+    int x = f(arr, i+1, n);
+    return arr[i] + x;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    cout<<f(arr, 0, n)<<endl;
+    return 0;
+}
