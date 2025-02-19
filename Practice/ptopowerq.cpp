6098f5d2662759f2ae5ad9f36cac378da44b7d32
myfirstrepo <abhaysingh6575@gmail.com>
2025-02-19 18:23:56 +0530

    alt. method for p to power q

--- Practice/ptopowerq.cpp
+++ Practice/ptopowerq.cpp
@@ -1,13 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int f(int p, int q){
+
+//     if(q == 0) return 1;
+
+//     int ans = pow(p, q);
+
+//     return ans;
+// }
+
+// int main() {
+//     int p, q;
+//     cin >> p >> q;
+//     cout << f(p, q) << endl;
+//     return 0;
+// }
+
+
 int f(int p, int q){
 
     if(q == 0) return 1;
 
-    int ans = pow(p, q);
-
-    return ans;
+    return p * f(p, q-1);
 }
 
 int main() {
