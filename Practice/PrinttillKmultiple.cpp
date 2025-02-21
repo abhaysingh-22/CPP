bbc9470d0b921940831485c52937adf8af56865e
myfirstrepo <abhaysingh6575@gmail.com>
2025-02-21 19:59:26 +0530

    To print K multiples of num

--- Practice/PrinttillKmultiple.cpp
+++ Practice/PrinttillKmultiple.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-void f(int n, int k, int curr = 1){
+/*void f(int n, int k, int curr = 1){
 
     if(curr > k){
         return;
@@ -13,6 +13,25 @@ void f(int n, int k, int curr = 1){
     f(n, k, curr+1);
 }
 
+
+int main(){
+    
+    int n, k;
+    cin >> n >> k;
+
+    f(n, k);
+    return 0;
+}*/
+
+void f(int n, int k){
+
+    if(k == 0){
+        return;
+    }
+
+    f(n, k-1);
+    cout << n * k << " ";
+}
 
 int main(){
     
