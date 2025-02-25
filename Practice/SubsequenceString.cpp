9c1c8e133ccd684400a7c4794ace07518440d2aa
myfirstrepo <abhaysingh6575@gmail.com>
2025-02-25 23:55:06 +0530

    Subsequence by recursion

--- Practice/SubsequenceString.cpp
+++ Practice/SubsequenceString.cpp
@@ -1,3 +1,6 @@
+// it is something like subsets of sets but here the relative order of the elements is preserved
+// for example: "abc" has 8 subsequences: "", "a", "b", "c", "ab", "bc", "ac", "abc"
+
 #include<bits/stdc++.h>
 using namespace std;
 
