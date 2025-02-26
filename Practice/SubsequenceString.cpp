ddb81a05e1107b58884b76e5def9d87962c695c9
myfirstrepo <abhaysingh6575@gmail.com>
2025-02-26 17:14:04 +0530

    Subsequence String by Recursion

--- Practice/SubsequenceString.cpp
+++ Practice/SubsequenceString.cpp
@@ -4,3 +4,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void f(string &str, int i, string result, vector<string> &li){
+    if(i == str.size()){
+        li.push_back(result);
+        return;
+    }
+    f(str, i+1, result, li);
+    f(str, i+1, result+str[i], li);
+}
+
+int main (){
+    string str = "abc";
+    vector<string> li;
+    f(str, 0, "", li);
+    for(auto x: li){
+        cout<<x<<endl;
+    }
+    return 0;
+}
+
+
+// just remember the above code and you can solve any question related to subsequence or subsets
