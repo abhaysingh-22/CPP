35ecdeecff961de1a7e3210031fd6ec954e2c870
myfirstrepo <abhaysingh6575@gmail.com>
2025-03-03 14:22:48 +0530

    Normal Counter question

---
+++ Practice/counter.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    while(n <= 1000){
+        cout << n << " ";
+        n++;
+    }
+
+    return 0;
+}
