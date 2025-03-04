e01018a3e0e23fbbd121813c29ab65222db529eb
myfirstrepo <abhaysingh6575@gmail.com>
2025-03-04 23:26:21 +0530

    updates in question

--- Practice/TwoSum.cpp
+++ Practice/TwoSum.cpp
@@ -1,8 +1,8 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
 
-                                                  // Approach 1: Using array
+//                                                   // Approach 1: Using array
 
 // int main(){
 
@@ -33,35 +33,86 @@ using namespace std;
 
 
 
-                                                     // Approach 2: Using vectors
+//                                                      // Approach 2: Using vectors
+
+// int main(){
+
+//     int n;
+//     cin >> n;
+//     vector<int> v(n);
+    
+
+//     for(int i =0; i < n; i++){
+//         cin >> v[i];
+//     }
+
+//     int target;
+//     cin >> target;
+
+
+//     for(int i = 0; i < n; i++){
+//         cout << v[i] << " ";
+//     }
+//     cout << endl;
+
+//     for(int i = 0; i < n; i++){
+//         for(int j = i+1; j < n; j++){
+//             if(v[i] + v[j] == target){
+//                 cout << i << " " << j << endl;
+//             }
+//         }
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> f(vector<int>& nums, int target){
+
+    vector<int> ans();
+    unordered_multiset<int>v;
+    for (auto it:nums)
+    {
+        v.insert(it);
+    }
+    for (auto it:v)
+    {
+        if (v.find(target-it)!=v.end())
+        {
+            ans.push_back(it);
+            ans.push_back(target-it);
+            break;
+        }
+    }
+    return ans;
+    
+}
 
 int main(){
 
-    int n;
-    cin >> n;
-    vector<int> v(n);
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        
     
-
-    for(int i =0; i < n; i++){
-        cin >> v[i];
-    }
-
-    int target;
-    cin >> target;
-
-
-    for(int i = 0; i < n; i++){
-        cout << v[i] << " ";
-    }
-    cout << endl;
-
-    for(int i = 0; i < n; i++){
-        for(int j = i+1; j < n; j++){
-            if(v[i] + v[j] == target){
-                cout << i << " " << j << endl;
-            }
+        for(int i =0; i < n; i++){
+            cin >> v[i];
         }
-    }
+    
+        int target;
+        cin >> target;
 
-    return 0;
+        vector<int> res = f(v, target);
+        for(int i = 0; i < res.size(); i++){
+            cout << res[i] << " ";
+        }
+        return 0;
 }
