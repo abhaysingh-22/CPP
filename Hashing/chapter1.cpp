25cb9916f01e50424f8bce56b8cd7d15b65f0d49
myfirstrepo <abhaysingh6575@gmail.com>
2025-03-27 15:35:27 +0530

    continue hashing

--- Hashing/chapter1.cpp
+++ Hashing/chapter1.cpp
@@ -10,9 +10,24 @@ int main(){
     dir["pradeep"] = 54;
     dir["himanshu"] = 64;
 
-    for(auto ele: dir){
-        cout << "Name- " << ele.first << endl;
-        cout << "Phone number- " << ele.second << endl;
+
+    // for(auto ele: dir){
+    //     cout << "Name- " << ele.first << endl;
+    //     cout << "Phone number- " << ele.second << endl;
+    // }
+
+
+    // map<string, int>::iterator it;
+    // for(it = dir.begin(); it != dir.end(); it++){
+    //     cout << "Name- " << it->first << endl;
+    //     cout << "Phone number- " << it->second << endl;
+    // }
+
+    
+    map<string, int>::reverse_iterator it;
+    for(it = dir.rbegin(); it != dir.rend(); it++){
+        cout << "Name- " << it->first << endl;
+        cout << "Phone number- " << it->second << endl;
     }
 
     return 0;
