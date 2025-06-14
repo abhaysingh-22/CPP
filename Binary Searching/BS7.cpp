2ff71fe0278e153e4c5fba60d4fa302259d782c7
myfirstrepo <abhaysingh6575@gmail.com>
2025-06-15 01:37:55 +0530

    binary search problem 7

---
+++ Binary Searching/BS7.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int peakMountain(vector<int> &arr) {
+    int n = arr.size();
+    int low = 0, high = n - 1;
+
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+
+        if(mid == 0){
+            if(arr[mid] > arr[mid + 1]){
+                return 0;
+            }else{
+                return 1;
+            }
+        }else if(mid == n - 1){
+            if(arr[mid] > arr[mid - 1]){
+                return n - 1;
+            }else{
+                return n - 2;
+            }
+        }else{
+            if(arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
+                return mid; 
+            } else if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1; 
+}
+
+int main(){
+    vector<int> arr = {1, 3, 5, 7, 6, 4, 2};
+    int peakIndex = peakMountain(arr);
+
+    if (peakIndex != -1) {
+        cout << "Peak element is at index: " << peakIndex << endl;
+        cout << "Peak element is: " << arr[peakIndex] << endl;
+    } else {
+        cout << "No peak element found." << endl;
+    }
+
+    return 0;
+}
