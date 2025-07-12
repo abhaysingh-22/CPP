0bba71fda8aa13b6039807b9baae72138f0b580e
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-12 13:16:08 +0530

    add implementation of doubly linked list with traversal and insertion at start

---
+++ DLL/TraverseDLL.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+class DoublyLinkedList{
+
+    public:
+    Node* head;
+    Node* tail;
+
+    DoublyLinkedList(){
+        head = NULL;
+        tail = NULL;
+    }
+
+    void insertAtStart(int val){
+
+        Node* new_node = new Node(val);
+        if(head == NULL){
+            head = new_node;
+            tail = new_node;
+            return;
+        }
+
+        new_node -> next = head;
+        head -> prev = new_node;
+        head = new_node;
+    }
+};
+
+int main(){
+
+    DoublyLinkedList dll;
+    dll.insertAtStart(10);
+    dll.insertAtStart(20);
+    dll.insertAtStart(30);
+
+    Node* current = dll.head;
+    cout << "Doubly Linked List values: ";
+    while(current != NULL){
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+
+    return 0;
+}
