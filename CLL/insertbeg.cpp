24348db3f3b843decf3c30071fa16aa04994229d
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-14 20:10:37 +0530

    add implementation of circular linked list with insert at start functionality

---
+++ CLL/insertbeg.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+
+public:
+    int val;
+    Node *next;
+
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+class CLL
+{
+
+public:
+    Node *head;
+
+    CLL()
+    {
+        head = NULL;
+    }
+
+    void Display()
+    {
+        if (head == NULL)
+            return;
+        Node *temp = head;
+        do
+        {
+            cout << temp->val << " ";
+            temp = temp->next;
+        } while (temp != head);
+        cout << endl;
+    }
+
+    void insertAtStart(int val)
+    {
+
+        Node *new_node = new Node(val);
+        if (head == NULL)
+        {
+            head = new_node;
+            new_node->next = head;
+            return;
+        }
+
+        Node *tail = head;
+        while (tail->next != head)
+        {
+            tail = tail->next;
+        }
+        tail->next = new_node;
+        new_node->next = head;
+        head = new_node;
+    }
+};
+
+int main()
+{
+
+    CLL cll;
+    cll.insertAtStart(3);
+    cll.insertAtStart(4);
+    cll.insertAtStart(5);
+    cll.insertAtStart(6);
+    cll.insertAtStart(7);
+    cll.Display();
+
+    return 0;
+}
