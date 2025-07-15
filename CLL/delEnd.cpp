c6be24a98db97af322e9600f48b4a733922a1429
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-15 19:44:54 +0530

    add implementation of delete node at end functionality in circular linked list

---
+++ CLL/delEnd.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+
+public:
+    int val;
+    Node *next;
+
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+class CLL
+{
+
+public:
+    Node *head;
+
+    CLL()
+    {
+        head = NULL;
+    }
+
+    void Display()
+    {
+        if (head == NULL)
+            return;
+        Node *temp = head;
+        do
+        {
+            cout << temp->val << " ";
+            temp = temp->next;
+        } while (temp != head);
+        cout << endl;
+    }
+
+    void insertAtEnd(int val)
+    {
+        Node *new_node = new Node(val);
+        if (head == NULL)
+        {
+            head = new_node;
+            new_node->next = head;
+            return;
+        }
+        Node *tail = head;
+        while (tail->next != head)
+        {
+            tail = tail->next;
+        }
+        tail->next = new_node;
+        new_node->next = head;
+    }
+
+    void delEnd()
+    {
+        if(head == NULL){
+            return;
+        }
+
+        Node* tail = head;
+
+        while(tail ->next -> next != head){
+            tail = tail -> next;
+        }
+
+        Node* temp = tail -> next;
+        tail->next = head;
+        delete temp;
+    }
+};
+
+int main()
+{
+
+    CLL cll;
+    cll.insertAtEnd(3);
+    cll.insertAtEnd(4);
+    cll.insertAtEnd(5);
+    cll.insertAtEnd(6);
+    cll.insertAtEnd(7);
+    cll.delEnd();
+    cll.Display();
+
+    return 0;
+}
